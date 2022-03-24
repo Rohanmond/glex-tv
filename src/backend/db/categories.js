d3b806d9c34d0d01c39a9a3599fe6136674d3633
Rohan Mondal <mondalrohan108@gmail.com>
2022-03-25 05:02:39 +0530

    added videos and categories

--- src/backend/db/categories.js
+++ src/backend/db/categories.js
@@ -1,27 +1,17 @@
-import { v4 as uuid } from "uuid";
-
 /**
  * Category Database can be added here.
  * You can add category of your wish with different attributes
  * */
 
 export const categories = [
-  {
-    _id: uuid(),
-    categoryName: "Computer Programming",
-    description:
-      "Computer programming is the process of designing and building an executable computer program to accomplish a specific computing",
-  },
-  {
-    _id: uuid(),
-    categoryName: "Frontend Development",
-    description:
-      "Front-end web development, also known as client-side development is the practice of producing HTML, CSS and JavaScript for a website or Web Application",
-  },
-  {
-    _id: uuid(),
-    categoryName: "Backend Development",
-    description:
-      "Backend Development is also known as server-side development. It is everything that the users don't see and contains behind-the-scenes activities that occur when performing any action on a website.",
-  },
+  { categoryName: 'action', _id: '0b510825-5dbb-434a-b5c4-6c178dc6dc53' },
+  { categoryName: 'crime', _id: '569a2ab5-7d9b-43f8-b5c8-a96776304366' },
+  { categoryName: 'drama', _id: '016e2f79-329f-41e6-9458-ceb0814a07db' },
+  { categoryName: 'adventure', _id: '22069658-a89a-4601-a909-6dd65b4dd375' },
+  { categoryName: 'sci-fi', _id: 'cf6a8dc5-8e4f-4223-9ec0-e16f03540c8a' },
+  { categoryName: 'thriller', _id: 'c8dcdd7d-d941-48bd-a8d6-4d3e85dc6d00' },
+  { categoryName: 'comedy', _id: '3ca1cafd-cadf-4cf1-9305-6bb89cb06c09' },
+  { categoryName: 'fantasy', _id: '27985cde-8df7-4ba3-9c2b-33ebf54d1a19' },
+  { categoryName: 'family', _id: '2d471bb0-5441-4895-b6ec-9b3c69e2947c' },
+  { categoryName: 'biography', _id: '36e87775-1471-478c-ae69-e04df96a2c99' },
 ];
--- src/backend/db/videos.js
+++ src/backend/db/videos.js
@@ -5,17 +5,141 @@
 
 export const videos = [
   {
-    _id: "Wo5dMEP_BbI",
-    title: "Awesome Video about Coding",
+    _id: '9EaBdOkMT_Q',
+    title: 'No time to die',
+    categories: ['action', 'crime', 'drama'],
     description:
-      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
-    creator: "Soham Shah",
+      'At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.',
+    creator: 'Amazon Prime Video India',
   },
   {
-    _id: "F_Riqjdh2oM",
-    title: "Neural Networks from Scratch - P.1 Intro and Neuron Code",
-    creator: "Sentdex",
+    _id: 'PLl99DlL6b4',
+    title: 'The Shashank redemption',
+    categories: ['drama'],
+    creator: 'Warner Bros. Entertainment',
     description:
-      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+      'At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.',
+  },
+  {
+    _id: 'LDG9bisJEaI',
+    title: 'The dark knight',
+    creator: 'DC',
+    categories: ['action', 'crime', 'drama'],
+    description:
+      'At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.',
+  },
+  {
+    _id: 'JE9z-gy4De4',
+    title: 'Inception',
+    categories: ['action', 'adventure', 'sci-fi'],
+    creator: 'Warner Bros. Entertainment',
+    description:
+      'At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.',
+  },
+  {
+    _id: 'qtRKdVHc-cE&t=16s',
+    title: 'Fight Club',
+    categories: ['drama'],
+    creator: 'Movieclips Classic Trailers',
+    description:
+      'At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.',
+  },
+  {
+    _id: 'zSWdZVtXT7E',
+    title: 'Insterstellar',
+    categories: ['adventure', 'drama', 'sci-fi'],
+    creator: 'Warner Bros UK & Ireland',
+    description:
+      'At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.',
+  },
+  {
+    _id: '5xH0HfJHsaY',
+    title: 'Parasite',
+    categories: ['drama', 'thriller'],
+    creator: 'IGN',
+    description:
+      'At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.',
+  },
+  {
+    _id: 'K0eDlFX9GMc',
+    title: '3 idiots',
+    categories: ['comedy', 'drama'],
+    creator: 'moviemaniacsDE',
+    description:
+      'At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.',
+  },
+  {
+    _id: 'JfVOs4VSpmA',
+    title: 'Spiderman-no way home',
+    categories: ['action', 'adventure', 'fantasy'],
+    creator: 'Sony Pictures Entertainment',
+    description:
+      'At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.',
+  },
+  {
+    _id: 'NyHpHF2AZuU',
+    title: 'Like stars on earth',
+    categories: ['drama', 'family'],
+    creator: 'YRF',
+    description:
+      'At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.',
+  },
+  {
+    _id: 'x_7YlGv9u1g',
+    title: 'Dangal',
+    categories: ['action', 'biography', 'drama'],
+    creator: 'UTV Motion Pictures',
+    description:
+      'At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.',
+  },
+  {
+    _id: 'SOXWc32k4zA',
+    title: 'PK',
+    categories: ['comedy', 'drama', 'sci-fi'],
+    creator: 'Movieclips India',
+    description:
+      'At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.',
+  },
+  {
+    _id: '-Qv6p6pTz5I',
+    title: 'Johny English Strikes Again',
+    categories: ['adventure', 'comedy'],
+    creator: 'universal pictures',
+    description: '',
+  },
+  {
+    _id: 'aEr6K1bwIVs&t=63s',
+    title: 'Steve Jobs',
+    categories: ['biography', 'drama'],
+    creator: 'universal pictures',
+    description: '',
+  },
+  {
+    _id: 'fb5ELWi-ekk',
+    title: 'Jurassic World Dominion',
+    categories: ['adventure', 'thriller'],
+    creator: 'universal pictures',
+    description: '',
+  },
+  {
+    _id: 'DqQe3OrsMKI',
+    title: 'THE GIRL WITH THE DRAGON TATTOO',
+    categories: ['adventure', 'thriller', 'action'],
+    creator: 'Sony Pictures Entertainment',
+    description: '',
+  },
+  {
+    _id: 'nuPZUUED5uk',
+    title: 'The Imitation Game',
+    categories: ['biography', 'drama'],
+    creator: 'Movieclips Trailer',
+    description: '',
+  },
+  {
+    _id: 'DMOBlEcRuw8',
+    title: 'The Pursuit of Happyness',
+    categories: ['family', 'adventure'],
+    creator: 'Movieclips Classic Trailers',
+    description: '',
   },
 ];
