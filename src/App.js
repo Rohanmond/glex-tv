25a6aff02580b23eedade10d1609d97582e0d56c
Rohan Mondal <mondalrohan108@gmail.com>
2022-03-25 17:39:31 +0530

    added nav , landing and home page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import { Route, Routes } from 'react-router-dom';
 import { useEffect } from 'react';
 import './App.css';
-import { MockAPI, Nav } from './components';
+import { Home, MockAPI, Nav } from './components';
 import { useData } from './contexts';
+import { LandingPage, VideoList } from './pages';
 
 function App() {
   const { state } = useData();
@@ -13,7 +14,11 @@ function App() {
     <>
       <Nav />
       <Routes>
+        <Route path='/' element={<LandingPage />} />
         <Route path='/mock-man' element={<MockAPI />} />
+        <Route path='/home' element={<Home />}>
+          <Route path='videos' element={<VideoList />} />
+        </Route>
       </Routes>
     </>
   );
