d6ff7c407eafb9c172cac04cb5fa10a6173d8c36
Rohan Mondal <mondalrohan108@gmail.com>
2022-03-26 16:11:22 +0530

    signup login added

---
+++ src/pages/Auth/Login/Login.jsx
@@ -0,0 +1,36 @@
+import '../auth.css';
+export const Login = () => {
+  return (
+    <div className='auth-outer-container'>
+      <div className='auth-box'>
+        <div className='auth-header font-wt-bold text-xl'>Login</div>
+        <form className='auth-form'>
+          <div className='auth-input-container'>
+            <label htmlFor='email'>Email</label>
+            <input className='auth-input' id='email' type='email' required />
+          </div>
+          <div className='auth-input-container'>
+            <label htmlFor='password'>Password</label>
+            <input
+              className='auth-input'
+              id='password'
+              type='password'
+              required
+            />
+          </div>
+
+          <div className='auth-input-btn-container'>
+            <input
+              className='btn btn-primary background-secondary brd-rd-semi-sq'
+              type='submit'
+              value={'signup'}
+            />
+          </div>
+          <div className='auth-link-container'>
+            <p className='font-wt-semibold'>Don't have an account? Signup</p>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,5 @@
 export { LandingPage } from './LandingPage/LandingPage';
 export { VideoList } from './VIdeoListPage/VideoList';
 export { VideoDetails } from './VideoDetails/VideoDetails';
+export { Signup } from './Auth/Signup/Signup';
+export { Login } from './Auth/Login/Login';
