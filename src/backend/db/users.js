f4ae7ddaaae89ff3df5dbc87c6cbe18476dad56c
Rohan Mondal <mondalrohan108@gmail.com>
2022-03-28 14:43:15 +0530

    fixed users

--- src/backend/db/users.js
+++ src/backend/db/users.js
@@ -9,8 +9,8 @@ import { formatDate } from '../utils/authUtils';
 export const users = [
   {
     _id: uuid(),
-    firstName: 'Adarsh',
-    lastName: 'Balika',
+    firstName: 'Rohan',
+    lastName: 'Mondal',
     email: 'rohan@gmail.com',
     password: '1234abcd',
     createdAt: formatDate(),
